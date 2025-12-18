135000e4be7b514193da8c3ad9966913e23a6e72
st20331094 <st20331094@S01110>
2025-12-18 14:54:41 +0000

    MINECRAFT BLOCK

--- gdv4002-base1/gdv4002-base1/main.cpp
+++ gdv4002-base1/gdv4002-base1/main.cpp
@@ -22,9 +22,8 @@ int main(void) {
 	//
 	// Setup game scene objects here
 	//
-	else {
-		printf("Engine initialised successfully.\n");
-	}
+	addObject("player", glm::vec2(1.0f, 1.0f), glm::radians(45.0f), glm::vec2(0.5f, 1.0f), "Resources\\Textures\\mcblock01.png", TextureProperties::NearestFilterTexture());
+	
 	
 	//
 	// Set callback functions
