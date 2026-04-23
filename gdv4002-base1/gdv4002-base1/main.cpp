26bf25005829439d9f62b811efb96d32dac22219
st20331094 <st20331094@S01110>
2026-04-23 11:54:24 +0100

    starting keyboard inputs

--- gdv4002-base1/gdv4002-base1/main.cpp
+++ gdv4002-base1/gdv4002-base1/main.cpp
@@ -1,11 +1,14 @@
 #include "Engine.h"
+#include "Keys.h"
+#include <bitset>
 
 
 // Function prototypes
 void myUpdate(GLFWwindow* window, double tDelta);
+void myKeyboardHandler(GLFWwindow* window, int key, int scancode, int action, int mods);
 
 // Global Variables
-
+std::bitset<5> keys{ 0x0 };
 
 int main(void) {
 
@@ -46,7 +49,7 @@ int main(void) {
 	// Set callback functions
 	//
 	setUpdateFunction(myUpdate);
-
+	setKeyboardHandler(myKeyboardHandler);
 
 
 	// Enter main loop - this handles update and render calls
@@ -76,6 +79,36 @@ void myUpdate(GLFWwindow* window, double tDelta) {
 
 
 }
+void myKeyboardHandler(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+// Check if the key was just pressed
+if (action == GLFW_PRESS) {
 	
+	// now check which key was pressed...
+	switch (key)
+	{
+	case GLFW_KEY_ESCAPE:
+			// If escape is pressed tell GLFW we want to close the window (and quit)
+			glfwSetWindowShouldClose(window, true);
+		break; 
+
+	case GLFW_KEY_W:
+		keys[Key::W] = true;
+		break;
+		}
+}
+// If not pressed, check the key has just been released
+else if (action == GLFW_RELEASE) {
+
+	// handle key release events
+	switch (key) {
+	
+	case GLFW_KEY_W:
+		keys[Key::W] = false;
+		break;
+		}
+	}
+}
+
 
 
