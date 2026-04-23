e9a68444b1f04d32e2ec8c6a74fe9508823069b7
st20331094 <st20331094@S01110>
2026-04-23 10:49:35 +0100

    added player and enemies

--- gdv4002-base1/gdv4002-base1/main.cpp
+++ gdv4002-base1/gdv4002-base1/main.cpp
@@ -18,16 +18,38 @@ int main(void) {
 		printf("Cannot setup game window!!!\n");
 		return initResult; // exit if setup failed
 	}
+	
+	
+
 
 	//
 	// Setup game scene objects here
 	//
+	// 
+	//addObject("player1", glm::vec2(-1.5f, 1.0f), glm::radians(-30.0f), glm::vec2(1.0f, 1.0f), "Resources\\Textures\\bumblebee.png");
+	//addObject("player2", glm::vec2(1.5f, 1.0f), glm::radians(45.0f), glm::vec2(1.0f, 1.0f), "Resources\\Textures\\bumblebee.png");
+	//float playerVelocity = 2.0f;
+	//float anglesPerSecond = glm::radians(45.0f);
+	//GameObject2D* player1 = getObject("player1");
+	//player1->orientation = player1->orientation + glm::radians(1.5);
+
+
+	addObject("player", glm::vec2(-1.5f, 0.0f), 0.0f, glm::vec2(0.5f, 0.5f), "Resources\\Textures\\player1_ship.png");
+
+	addObject("enemy", glm::vec2(0.0f, 0.0f), 0.0f, glm::vec2(0.75f, 0.75f), "Resources\\Textures\\alien01.png");
+
+	addObject("enemy", glm::vec2(1.0f, 0.0f), 0.0f, glm::vec2(0.75f, 0.75f), "Resources\\Textures\\alien01.png");
+
+	addObject("enemy", glm::vec2(2.0f, 0.0f), 0.0f, glm::vec2(0.75f, 0.75f), "Resources\\Textures\\alien01.png");
 
 	
 	//
 	// Set callback functions
 	//
-	
+	void myUpdate(GLFWwindow * window, double tDelta) 
+		
+		; float player1RotationSpeed = glm::radians(90.0f);
+
 
 	// Enter main loop - this handles update and render calls
 	engineMainLoop();
